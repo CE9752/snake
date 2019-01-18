3a1540df0a773fe8cf57d1af7f45fa495fe66681
CE9752 <45814429+CE9752@users.noreply.github.com>
2019-01-18 22:53:52 +0330

    boxes added

---
+++ box.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL2_gfxPrimitives.h>
+using namespace std;
+class Box{
+	public:
+		int tol= 50;
+		int arz = 50;
+		int x_pos;
+		int y_pos;	
+		int jon;
+		bool show = false;
+		void set_jon(int Jon){
+			jon = Jon;
+		}
+		void set_position(int i_x, int j_y){
+			x_pos = tol*i_x;
+			y_pos = arz*j_y;
+		}
+
+		void is_touched()
+		{
+			jon--;
+		}
+		void block(){
+		 	boxRGBA(r,x_pos,y_pos,x_pos+48,y_pos+50,0,250,0,255);
+		}
+};
--- snake.cpp
+++ snake.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 #include<SDL2/SDL.h>
+#include<SDL2/SDL2_gfxPrimitives.h>
 #include<ctime>
-using namespace std;
-const int SCREEN_WIDTH=600;
-const int SCREEN_HEIGHT=700;
+#include<cstdlib>
 SDL_Window* w=NULL;
 SDL_Surface* s=NULL;
 SDL_Renderer* r = NULL;
+#include"box.h"
+using namespace std;
+const int SCREEN_WIDTH=600;
+const int SCREEN_HEIGHT=700;
 bool init(){
     bool success = true;
     w = SDL_CreateWindow("win",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
@@ -30,18 +33,57 @@ void close(){
     SDL_Quit();
 }
 int main(){
+	srand(time(0));
+	float j=0;
     if(!init()){
         cout<<"failed to initialize!";
     }
     else{
+    	Box boxes[12][14];
+    	int count=0;
     	SDL_Event e;
     	bool quit = false;
     	while(!quit){
+    		SDL_SetRenderDrawColor(r,0,0,0,255);
+ 			SDL_RenderClear(r);
+    		// if(count==0){
+    		// 	for(int i=0;i<=rand()%12;i++){
+    		// 		boxes[rand()%12][].show=true;
+    		// 	}
+    		// 	count++;
+    		// }
+    		// for(int i=0;i<12;i++){
+    		// 	if(boxes[i].show==true){
+    		// 		if(j<=650){
+    		// 			boxes[i].set_position(i,j);
+    		// 			boxes[i].block();
+    		// 		}
+    		// 	}
+    		// }
+    		for(int j=0;j<14;j++){
+ 				if(count==0){
+ 					for(int i=0;i<=rand()%12;i++){
+ 						boxes[rand()%12][j].show=true;
+ 					}
+ 					count++;
+ 				}
+ 				for(int i=0;i<12;i++){
+ 					if(boxes[i][j].show==true){
+ 						boxes[i][j].set_position(i,j);
+ 						boxes[i][j].block();
+ 						boxes[i][j+1].show=true;
+ 						//boxes[i][j].show=false;
+ 					}
+ 				}
+ 			}
+
     		while(SDL_PollEvent(&e) !=0){
                 if(e.type == SDL_QUIT ){
                     quit = true;
                 }
             }
+            SDL_RenderPresent(r);
+           // j=j+0.1;
     	}
     }
     close();
